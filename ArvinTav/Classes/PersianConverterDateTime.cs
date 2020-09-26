9336265da5f0ec91018eac2da42629f0774358ab
Hadi sadegh <hadisdegh@yahoo.com>
2020-09-26 10:22:07 +0330

    Add_CkEditor_And_PersianDateTime

---
+++ ArvinTav/Classes/PersianConverterDateTime.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Globalization;
+
+namespace ArazCode
+{
+    public static class PersianConverterDateTime
+    {
+        public static string ToShamsi(this DateTime value)
+        {
+            PersianCalendar Pc = new PersianCalendar();
+
+            return " " + Pc.GetYear(value) + "/" + Pc.GetMonth(value).ToString("00") + "/" + Pc.GetDayOfMonth(value).ToString("00") + "-" + Pc.GetHour(value).ToString("00") + ":" + Pc.GetMinute(value).ToString("00");
+        }
+    }
+}

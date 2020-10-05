c0d0e2fb7cb58da26f3f9407ac34d63cf3e89433
Hadi sadegh <hadisdegh@yahoo.com>
2020-10-05 10:31:00 +0330

    Create Dashboard in Admin

--- ArvinTav/Areas/Admin/Controllers/DashboardController.cs
+++ ArvinTav/Areas/Admin/Controllers/DashboardController.cs
@@ -9,10 +9,38 @@ namespace ArvinTav.Areas.Admin.Controllers
 {
     public class DashboardController : Controller
     {
-        ArvinContext db = new ArvinContext();
+        private IServiceCategoryRepository serviceCategoryRepository;
+        private IDomainRepository domainRepository;
+        private IHostingRepository HostingRepository;
+        private IPackageRepository packageRepository;
+        private IUserRepository userRepository;
+        private IOrderRepository orderRepository;
+        private ITicketRepository ticketRepository;
+        private ArvinContext db = new ArvinContext();
+
+        public DashboardController()
+        {
+            serviceCategoryRepository = new ServiceCategoryRepository(db);
+            domainRepository = new DomainRepository(db);
+            HostingRepository = new HostingRepository(db);
+            packageRepository = new PackageRepository(db);
+            userRepository = new UserRepository(db);
+            orderRepository = new OrderRepository(db);
+            ticketRepository = new TicketRepository(db);
+        }
+
         // GET: Admin/Dashboard
         public ActionResult Index()
         {
+            ViewBag.User = userRepository.AllUser().Where(u => u.UserRole.Name == "User").Count();
+            ViewBag.Personnel = userRepository.AllUser().Where(u => u.UserRole.Name == "Content" || u.UserRole.Name == "Marketer" || u.UserRole.Name == "Manager" || u.UserRole.Name == "Admin").Count();
+            ViewBag.Host = HostingRepository.AllhostingServices().Count();
+            ViewBag.Domain = domainRepository.AllDomain(false).Count();
+            ViewBag.Package = packageRepository.AllPackageServices().Count();
+            ViewBag.Order = orderRepository.AllOrders().Count();
+            ViewBag.Ticket = ticketRepository.AllTicket().Count();
+            ViewBag.TicketEnded = ticketRepository.AllTicket().Where(t => t.Status == 4).Count();
+            ViewBag.TicketNotSeen = ticketRepository.AllTicket().Where(t => t.Status == 1).Count();
             return View();
         }
     }
---
+++ DataLayer/Services/TicketRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class TicketRepository : ITicketRepository
+    {
+        private ArvinContext db;
+
+        public TicketRepository(ArvinContext context)
+        {
+            this.db = context;
+        }
+        public IEnumerable<Ticket> AllTicket()
+        {
+            return db.tickets;
+        }
+    }
+}

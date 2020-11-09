ce4c6cc4aa7bc186ac73f0ec96e60eb2467f3da5
Hadi sadegh <hadisdegh@yahoo.com>
2020-11-09 18:31:20 +0330

    debug erorr

--- DataLayer/Models/PackageServiceDetail.cs
+++ DataLayer/Models/PackageServiceDetail.cs
@@ -1,9 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-<<<<<<< HEAD
-
-=======
->>>>>>> 5a943b0ca43ae54c17807f00b086e5fb9aa6ea2d
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;

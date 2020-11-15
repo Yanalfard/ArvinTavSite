2d26190905e1b2cc6f739f7e0d1e597f7b508e22
Hadi sadegh <hadisdegh@yahoo.com>
2020-11-15 10:37:15 +0330

    debug2innerticket

---
+++ DataLayer/Migrations/202011150705596_debuginnertickettbl.cs
@@ -0,0 +1,22 @@
+﻿namespace DataLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class debuginnertickettbl : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.InnerTickets", "SideID", c => c.Int());
+            AlterColumn("dbo.PackageServices", "Description", c => c.String(maxLength: 1000));
+            DropColumn("dbo.InnerTickets", "ParentID");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.InnerTickets", "ParentID", c => c.Int());
+            AlterColumn("dbo.PackageServices", "Description", c => c.String());
+            DropColumn("dbo.InnerTickets", "SideID");
+        }
+    }
+}

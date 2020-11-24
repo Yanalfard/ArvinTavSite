0d838fca26349d231ce424c91f919dc128ec0f16
nazila bagheri <nazila.bagheri93@gmail.com>
2020-11-24 14:29:32 +0330

    front end debug

--- ArvinTav/Classes/Sms.cs
+++ ArvinTav/Classes/Sms.cs
@@ -13,7 +13,7 @@ namespace ArvinTav
             var receptor = PhonNumber;
             var message = Massage;
             var api = new Kavenegar.KavenegarApi("4D4B66616C686B64534544333856706F7A6A35793647497735395A79496C59485644345257546C615137303D");
-            api.Send(sender, receptor, message);
+            api.VerifyLookup(receptor, "hello", "verifecation");
             return true;
         }
     }
